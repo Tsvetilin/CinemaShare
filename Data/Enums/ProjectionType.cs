92dcb027ddb96cbe3e9cf7891be3da20082cc298
Tony5768 <tonkatazl@gmail.com>
2020-05-17 00:38:47 +0300

    Fixed some details

--- Data/Enums/ProjectionType.cs
+++ Data/Enums/ProjectionType.cs
@@ -1,9 +1,15 @@
-﻿namespace Data.Enums
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Data.Enums
 {
+    
     public enum ProjectionType
     {
+        [Display(Name ="2D")]
         _2D,
+        [Display(Name = "3D")]
         _3D,
+        [Display(Name ="4D")]
         _4D,
     }
 }
