efe486a9d341972c3e554eabf4f44d562317dc14
Tony5768 <tonkatazl@gmail.com>
2020-05-19 10:23:41 +0300

    Added business logic tests

---
+++ Tests/Business.Tests/ReviewBusinessTests.cs
@@ -0,0 +1,84 @@
+﻿using Moq;
+using NUnit.Framework;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Business;
+using Data;
+using Data.Models;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Threading;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using System;
+using Data.Enums;
+using CinemaShare.Common.Mapping;
+using CinemaShare.Models.ViewModels;
+
+namespace Tests.Business.Tests
+{
+    class ReviewBusinessTests
+    {
+        [Test]
+        public async Task AddAsyncAddsElementToDb()
+        {
+            // Arrange
+            var reviews = new List<FilmReview>
+            {
+                new FilmReview {},
+                new FilmReview {}
+            }.AsQueryable();
+
+            var mockSet = new Mock<DbSet<FilmReview>>();
+            mockSet.As<IQueryable<FilmReview>>().Setup(m => m.Provider).Returns(reviews.Provider);
+            mockSet.As<IQueryable<FilmReview>>().Setup(m => m.Expression).Returns(reviews.Expression);
+            mockSet.As<IQueryable<FilmReview>>().Setup(m => m.ElementType).Returns(reviews.ElementType);
+            mockSet.As<IQueryable<FilmReview>>().Setup(m => m.GetEnumerator()).Returns(reviews.GetEnumerator());
+
+            var mockContext = new Mock<CinemaDbContext>();
+            mockContext.Setup(c => c.FilmReviews).Returns(mockSet.Object);
+            mockContext.Setup(m => m.AddAsync(It.IsAny<FilmReview>(), It.IsAny<CancellationToken>()))
+                .Returns(new ValueTask<EntityEntry<FilmReview>>(Task.FromResult((EntityEntry<FilmReview>)null)));
+
+            var filmReviewBusiness = new FilmReviewBusiness(mockContext.Object);
+            var mapper = new Mapper();
+            var filmReview = new FilmReview { };
+
+            // Act
+            await filmReviewBusiness.Add(filmReview);
+
+            // Assert
+            mockSet.Verify(m => m.AddAsync(It.IsAny<FilmReview>(), It.IsAny<CancellationToken>()), Times.Once());
+            mockContext.Verify(m => m.SaveChangesAsync(It.IsAny<CancellationToken>()), Times.Once());
+        }
+
+        [Test]
+        public async Task GetAsyncReturnsElementFromDb()
+        {
+            // Arrange
+            var reviews = new List<FilmReview>
+            {
+                new FilmReview {},
+                new FilmReview {}
+            }.AsQueryable();
+
+            var mockSet = new Mock<DbSet<FilmReview>>();
+            mockSet.As<IQueryable<FilmReview>>().Setup(m => m.Provider).Returns(reviews.Provider);
+            mockSet.As<IQueryable<FilmReview>>().Setup(m => m.Expression).Returns(reviews.Expression);
+            mockSet.As<IQueryable<FilmReview>>().Setup(m => m.ElementType).Returns(reviews.ElementType);
+            mockSet.As<IQueryable<FilmReview>>().Setup(m => m.GetEnumerator()).Returns(reviews.GetEnumerator());
+
+            var mockContext = new Mock<CinemaDbContext>();
+            mockContext.Setup(c => c.FilmReviews).Returns(mockSet.Object);
+            mockContext.Setup(c => c.FilmReviews.FindAsync(It.IsAny<string>())).Returns(new ValueTask<FilmReview>((reviews.First())));
+
+
+            var filmReviewBusiness = new FilmReviewBusiness(mockContext.Object);
+
+            // Act
+            var filmReview = await filmReviewBusiness.Get(reviews.First().Id);
+
+            // Assert
+            Assert.AreEqual(reviews.First().Id, filmReview.Id, "Doesn't return searched element from database");
+        }
+    }
+}
--- Tests/Business.Tests/TicketBusinessTests.cs
+++ Tests/Business.Tests/TicketBusinessTests.cs
@@ -2,12 +2,17 @@
 using NUnit.Framework;
 using System.Collections.Generic;
 using Microsoft.EntityFrameworkCore;
+using Business;
 using Data;
 using Data.Models;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Threading;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using System;
+using Data.Enums;
 using CinemaShare.Common.Mapping;
-using Business;
+using CinemaShare.Models.ViewModels;
 
 namespace Tests.Business.Tests
 {
@@ -71,6 +76,158 @@ namespace Tests.Business.Tests
 
             // Assert
             Assert.AreEqual(2, resultTicketsCount, "Doesn't return all elements in the database.");
+        }
+
+        [Test]
+        public async Task AddMultipleAddsRangeOfTickets()
+        {
+            // Arrange
+            var tickets = new List<ProjectionTicket>
+            {
+                new ProjectionTicket { },
+                new ProjectionTicket { },
+            }.AsQueryable();
+
+            var mockSet = new Mock<DbSet<ProjectionTicket>>();
+            mockSet.As<IQueryable<ProjectionTicket>>().Setup(m => m.Provider).Returns(tickets.Provider);
+            mockSet.As<IQueryable<ProjectionTicket>>().Setup(m => m.Expression).Returns(tickets.Expression);
+            mockSet.As<IQueryable<ProjectionTicket>>().Setup(m => m.ElementType).Returns(tickets.ElementType);
+            mockSet.As<IQueryable<ProjectionTicket>>().Setup(m => m.GetEnumerator()).Returns(tickets.GetEnumerator());
+
+            var mockContext = new Mock<CinemaDbContext>();
+            mockContext.Setup(c => c.ProjectionTickets).Returns(mockSet.Object);
+            mockContext.Setup(m => m.AddAsync(It.IsAny<ProjectionTicket>(), It.IsAny<CancellationToken>()))
+               .Returns(new ValueTask<EntityEntry<ProjectionTicket>>(Task.FromResult((EntityEntry<ProjectionTicket>)null)));
+            mockContext.Setup(c => c.ProjectionTickets.FindAsync(It.IsAny<string>())).Returns(new ValueTask<ProjectionTicket>((tickets.First())));
+
+            var projectionTicketBusiness = new ProjectionTicketBusiness(mockContext.Object);
+            var projectionTickets = new List<ProjectionTicket> { new ProjectionTicket { }, new ProjectionTicket { } };
+            // Act
+            await projectionTicketBusiness.AddMultipleAsync(projectionTickets);
+
+            // Assert 
+            mockSet.Verify(m => m.AddAsync(It.IsAny<ProjectionTicket>(), It.IsAny<CancellationToken>()), Times.AtLeastOnce());
+            mockContext.Verify(m => m.SaveChangesAsync(It.IsAny<CancellationToken>()), Times.Once());
+        }
+
+        [Test]
+        public void GetProjectionAndUserReturnsAllTicketsInTheEnteredProjectionAndItsCreater()
+        {
+            // Arrange
+            var tickets = new List<ProjectionTicket>
+            {
+                new ProjectionTicket {Projection= new FilmProjection{Id="1" }, HolderId="User1" },
+                new ProjectionTicket {Projection= new FilmProjection{Id="1" }, HolderId="User1"},
+            }.AsQueryable();
+
+            var mockSet = new Mock<DbSet<ProjectionTicket>>();
+            mockSet.As<IQueryable<ProjectionTicket>>().Setup(m => m.Provider).Returns(tickets.Provider);
+            mockSet.As<IQueryable<ProjectionTicket>>().Setup(m => m.Expression).Returns(tickets.Expression);
+            mockSet.As<IQueryable<ProjectionTicket>>().Setup(m => m.ElementType).Returns(tickets.ElementType);
+            mockSet.As<IQueryable<ProjectionTicket>>().Setup(m => m.GetEnumerator()).Returns(tickets.GetEnumerator());
+
+            var mockContext = new Mock<CinemaDbContext>();
+            mockContext.Setup(c => c.ProjectionTickets).Returns(mockSet.Object);
+            mockContext.Setup(c => c.ProjectionTickets.FindAsync(It.IsAny<string>())).Returns(new ValueTask<ProjectionTicket>((tickets.First())));
+
+            var projectionTicketBusiness = new ProjectionTicketBusiness(mockContext.Object);
+
+            // Act
+            var resultTickets = projectionTicketBusiness.GetForProjectionAndUser(tickets.First().Projection.Id, tickets.First().HolderId);
+
+            // Assert 
+            Assert.AreEqual(2, resultTickets.Count(), "Does't return all tickets with entered holder ID");
+        }
+
+        [Test]
+        public void GetForUserReturnsAllTicketHoldedByTheEnteredUser()
+        {
+            // Arrange
+            var tickets = new List<ProjectionTicket>
+            {
+                new ProjectionTicket {Holder=new CinemaUser{ },Price=5, HolderId="User1", Projection=new FilmProjection{Cinema=new Cinema{Name="DEF" }, Film =new Film{ FilmData= new FilmData{ Title="XXX"} } },ReservedOn=DateTime.Now, Seat=1},
+                new ProjectionTicket{Holder=new CinemaUser{ },Price=8, HolderId="User1", Projection=new FilmProjection{Cinema=new Cinema{Name="ABC" }, Film =new Film{ FilmData= new FilmData{ Title="YYY"} } },ReservedOn=DateTime.Now, Seat=2},
+            }.AsQueryable();
+
+            var mockSet = new Mock<DbSet<ProjectionTicket>>();
+            mockSet.As<IQueryable<ProjectionTicket>>().Setup(m => m.Provider).Returns(tickets.Provider);
+            mockSet.As<IQueryable<ProjectionTicket>>().Setup(m => m.Expression).Returns(tickets.Expression);
+            mockSet.As<IQueryable<ProjectionTicket>>().Setup(m => m.ElementType).Returns(tickets.ElementType);
+            mockSet.As<IQueryable<ProjectionTicket>>().Setup(m => m.GetEnumerator()).Returns(tickets.GetEnumerator());
+
+            var mockContext = new Mock<CinemaDbContext>();
+            mockContext.Setup(c => c.ProjectionTickets).Returns(mockSet.Object);
+            mockContext.Setup(c => c.ProjectionTickets.FindAsync(It.IsAny<string>())).Returns(new ValueTask<ProjectionTicket>((tickets.First())));
+
+            var projectionTicketBusiness = new ProjectionTicketBusiness(mockContext.Object);
+            var mapper = new Mapper();
+            // Act
+            var resultTickets = projectionTicketBusiness.GetForUser(tickets.First().HolderId, mapper.MapToTicketCardViewModel);
+
+            // Assert 
+            Assert.AreEqual(tickets.First().Price, resultTickets.First().Price, "Does't return all tickets with entered holder ID");
+        }
+
+        [Test]
+        public async Task DeleteRemovesSelectedElement()
+        {
+            // Arrange
+            var tickets = new List<ProjectionTicket>
+            {
+                new ProjectionTicket { },
+                new ProjectionTicket{ },
+            }.AsQueryable();
+
+            var mockSet = new Mock<DbSet<ProjectionTicket>>();
+            mockSet.As<IQueryable<ProjectionTicket>>().Setup(m => m.Provider).Returns(tickets.Provider);
+            mockSet.As<IQueryable<ProjectionTicket>>().Setup(m => m.Expression).Returns(tickets.Expression);
+            mockSet.As<IQueryable<ProjectionTicket>>().Setup(m => m.ElementType).Returns(tickets.ElementType);
+            mockSet.As<IQueryable<ProjectionTicket>>().Setup(m => m.GetEnumerator()).Returns(tickets.GetEnumerator());
+
+            var mockContext = new Mock<CinemaDbContext>();
+            mockContext.Setup(c => c.ProjectionTickets).Returns(mockSet.Object);
+            mockContext.Setup(c => c.ProjectionTickets.FindAsync(It.IsAny<string>())).Returns(new ValueTask<ProjectionTicket>((tickets.First())));
+            mockContext.Setup(m => m.Remove(It.IsAny<ProjectionTicket>())).Returns((EntityEntry<ProjectionTicket>)null);
+
+            var projectionTicketBusiness = new ProjectionTicketBusiness(mockContext.Object);
+            
+            // Act
+            await projectionTicketBusiness.DeleteAsync(tickets.First().Id);
+
+            // Assert 
+            mockSet.Verify(m => m.Remove(It.IsAny<ProjectionTicket>()), Times.Once());
+            mockContext.Verify(m => m.SaveChangesAsync(It.IsAny<CancellationToken>()), Times.Once());
+        }
+
+        [Test]
+        public async Task DeleteRemovesNothing()
+        {
+            // Arrange
+            var tickets = new List<ProjectionTicket>
+            {
+                new ProjectionTicket { },
+                new ProjectionTicket{ },
+            }.AsQueryable();
+
+            var mockSet = new Mock<DbSet<ProjectionTicket>>();
+            mockSet.As<IQueryable<ProjectionTicket>>().Setup(m => m.Provider).Returns(tickets.Provider);
+            mockSet.As<IQueryable<ProjectionTicket>>().Setup(m => m.Expression).Returns(tickets.Expression);
+            mockSet.As<IQueryable<ProjectionTicket>>().Setup(m => m.ElementType).Returns(tickets.ElementType);
+            mockSet.As<IQueryable<ProjectionTicket>>().Setup(m => m.GetEnumerator()).Returns(tickets.GetEnumerator());
+
+            var mockContext = new Mock<CinemaDbContext>();
+            mockContext.Setup(c => c.ProjectionTickets).Returns(mockSet.Object);
+            mockContext.Setup(c => c.ProjectionTickets.FindAsync(It.IsAny<string>())).Returns(new ValueTask<ProjectionTicket>((ProjectionTicket)null));
+            mockContext.Setup(m => m.Remove(It.IsAny<ProjectionTicket>())).Returns((EntityEntry<ProjectionTicket>)null);
+
+            var projectionTicketBusiness = new ProjectionTicketBusiness(mockContext.Object);
+
+            // Act
+            await projectionTicketBusiness.DeleteAsync(tickets.First().Id);
+
+            // Assert 
+            mockSet.Verify(m => m.Remove(It.IsAny<ProjectionTicket>()), Times.Never());
+            mockContext.Verify(m => m.SaveChangesAsync(It.IsAny<CancellationToken>()), Times.Never());
         }
     }
 }
