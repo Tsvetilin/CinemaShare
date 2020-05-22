405f63f8d31ea50c68289218b72ce5766f31e152
pavlin1004 <47292086+pavlin1004@users.noreply.github.com>
2020-05-22 15:21:49 +0300

    Added documentation for CinemaBusiness

--- Business/CinemaBusiness.cs
+++ Business/CinemaBusiness.cs
@@ -43,29 +43,52 @@ namespace Business
         /// <summary>
         /// Gets all the cinemas in the database
         /// </summary>
-        /// <returns>Returns all the cinemas in the database</returns>
+        /// <returns>All cinemas</returns>
         public IEnumerable<Cinema> GetAll()
         {
             return context.Cinemas.ToList();
         }
-
+        
+        
+        /// <returns>
+        ///The count of all cinemas in the database
+        ///</returns>
         public int CountAllCinemas()
         {
             return context.Cinemas.Count();
         }
+        
+        /// <summary>
+        /// Gets all the cinema from searched page.
+        /// </summary>
+        /// <param name = "page">The number of the page</param>
+        /// <param name = "cinemasOnPage">The number of the cinemas on the searched page</param>
+        /// <returns>The cinemas on the selected page</returns>
         public IEnumerable<TModel> GetPageItems<TModel>(int page, int cinemasOnPage, Func<Cinema, TModel> mapToModelFunc)
         {
             var cinemas = GetAll();
             var selectedCinemas = cinemas.Skip(cinemasOnPage * (page - 1)).Take(cinemasOnPage);
             return selectedCinemas.Select(x => mapToModelFunc(x));
         }
-
+        
+        /// <summary>
+        /// Gets cinema by ID.
+        /// </summary>
+        /// <param name = "id">The ID of the cinema</param>
+        /// <returns>The cinema as a TModel</returns>
         public async Task<TModel> GetAsync<TModel>(string id, Func<Cinema, TModel> mapToModelFunc)
         {
             var cinema = await context.Cinemas.FindAsync(id);
             return mapToModelFunc(cinema);
         }
 
+        /// <summary>
+        /// Delete a cinema from the database,
+        /// reject all the projections in the cinema 
+        /// and send email to people who had reserved tickets to cancel their reservations
+        /// </summary>
+        /// <param name="id">The id of the cinema</param>
+        /// <param name="id">The URL of the projection</param>
         public async Task UpdateAsync(Cinema cinema, string ticketUrlPattern)
         {
             var cinemaInContext = await context.Cinemas.FindAsync(cinema.Id);
@@ -84,7 +107,14 @@ namespace Business
                 await context.SaveChangesAsync();
             }
         }
-
+        
+        /// <summary>
+        /// Delete a cinema from the database,
+        /// reject all the projections in the cinema 
+        /// and send email to people who had reserved tickets to cancel their reservations
+        /// </summary>
+        /// <param name="id">The id of the cinema.</param>
+        /// <param name="id">The URL of hte projection.</param>
         public async Task DeleteAsync(string id, string projectionsUrlPattern)
         {
             var cinemaInContext = await context.Cinemas.FindAsync(id);
@@ -103,24 +133,42 @@ namespace Business
                 await context.SaveChangesAsync();
             }
         }
-
+         /// <summary>
+        /// Checks if cinema is already added to the database
+        /// </summary>
+        /// <param name="cinemaName">The name of the searched cinema</param>
+        /// <returns>True, if the name of the cinema exists, or false if not</returns>
         public bool IsAlreadyAdded(string cinemaName)
         {
             return context.Cinemas.Any(x => x.Name.ToLower().Equals(cinemaName.ToLower()));
         }
 
+        /// <summary>
+        /// Gets all the cinemas in the searched city
+        /// </summary>
+        /// <param name="searchString">The name of the city</param>
+        /// <returns>All the cinemas with the searched name</returns>
         public IEnumerable<TModel> GetAllByName<TModel>(string searchString, Func<Cinema, TModel> mapToModelFunc)
         {
             return GetAll().Where(x => x.Name.ToLower().Contains(searchString.ToLower()))
                                                 .Select(x => mapToModelFunc(x)).ToList();
         }
-
+        
+        /// <summary>
+        /// Gets all the cinemas in the searched city
+        /// </summary>
+        /// <param name="searchString">The name of the city</param>
+        /// <returns>A list with all the cinema names from the searched city</returns>
         public IEnumerable<TModel> GetAllByCity<TModel>(string searchString, Func<Cinema, TModel> mapToModelFunc)
         {
             return GetAll().Where(x => x.City.ToLower().Contains(searchString.ToLower()))
                                                 .Select(x => mapToModelFunc(x)).ToList();
         }
-
+        /// <summary>
+        /// Gets the results from searching by name or city.
+        /// </summary>
+        /// <param name="searchString">The name of the city or the name of the cinema </param>
+        /// <returns>The results from searching</returns>
         public IEnumerable<TModel> GetSearchResults<TModel>(string searchString, Func<Cinema, TModel> mapToModelFunc)
         {
             var result = new List<TModel>();
