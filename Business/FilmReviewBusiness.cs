2c0f140d031ad64594b07fafd3c62c4c9438f52c
pavlin1004 <47292086+pavlin1004@users.noreply.github.com>
2020-05-22 17:37:04 +0300

    Added documentation for FilmReviewBusiness

--- Business/FilmReviewBusiness.cs
+++ Business/FilmReviewBusiness.cs
@@ -9,28 +9,47 @@ namespace Business
     public class FilmReviewBusiness : IFilmReviewBusiness
     {
         private readonly CinemaDbContext context;
-
+        
         public FilmReviewBusiness(CinemaDbContext context)
         {
             this.context = context;
         }
 
+        /// <summary>
+        /// Adds a new review
+        /// </summary>
+        /// <param name="filmReview">New review</param>
+        /// <returns></returns>
         public async Task Add(FilmReview filmReview)
         {
             await context.FilmReviews.AddAsync(filmReview);
             await context.SaveChangesAsync();
         }
 
+        /// <summary>
+        /// Finds a review bt ID
+        /// </summary>
+        /// <param name="cinema">The ID of the review</param>
+        /// <returns>Review</returns>
         public async Task<FilmReview> Get(string id)
         {
             return await context.FilmReviews.FindAsync(id);
         }
 
+        /// <summary>
+        /// Gets all the reviews
+        /// </summary>
+        /// <returns>List with reviews </returns>
         public async Task<IEnumerable<FilmReview>> GetAll()
         {
             return await context.FilmReviews.ToListAsync();
         }
 
+        /// <summary>
+        /// Update the selected review if exists
+        /// </summary>
+        /// <param name="filmReview">A review of the film</param>
+        /// <returns></returns>
         public async Task Update(FilmReview filmReview)
         {
             var filmInContext = await context.FilmReviews.FindAsync(filmReview.Id);
@@ -41,6 +60,11 @@ namespace Business
             }
         }
 
+        /// <summary>
+        /// Delete a review with the selected ID if exists
+        /// </summary>
+        /// <param name="id">Review ID</param>
+        /// <returns></returns>
         public async Task Delete(string id)
         {
             var filmInContext = await context.FilmReviews.FindAsync(id);
